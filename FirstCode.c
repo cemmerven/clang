5a21736bf8e468d48b46c0cc685aa197691106fb
cemmerven <cemmerven@gmail.com>
2015-07-24 14:55:13 +0300

    update

--- FirstCode.c
+++ FirstCode.c
@@ -20,57 +20,7 @@
 #include <stdlib.h>
 
 #include <stdbool.h>
-
-//-----------------------------------------------------------------------------
-
-
-/*  Return the ULP of q.
-
-    This was inspired by Algorithm 3.5 in Siegfried M. Rump, Takeshi Ogita, and
-    Shin'ichi Oishi, "Accurate Floating-Point Summation", _Technical Report
-    05.12_, Faculty for Information and Communication Sciences, Hamburg
-    University of Technology, November 13, 2005.
-*/
-float ULP( float q ) {
-
-    // SmallestPositive is the smallest positive floating-point number.
-    static const float SmallestPositive = FLT_EPSILON * FLT_MIN;
-
-    /*  Scale is .75 ULP, so multiplying it by any significand in [1, 2) yields
-        something in [.75 ULP, 1.5 ULP) (even with rounding).
-    */
-    static const float Scale = 0.75 * FLT_EPSILON;
-
-    q = fabs(q);
-
-    // link with linker command line option -lm (Link Math library) otherwise fmax goes undefined
-    return fmax(SmallestPositive, q - (q - q * Scale));
-
-}//ULP
-
-//-----------------------------------------------------------------------------
-
-/*  Return the next floating-point value after the finite value q.
-
-    This was inspired by Algorithm 3.5 in Siegfried M. Rump, Takeshi Ogita, and
-    Shin'ichi Oishi, "Accurate Floating-Point Summation", _Technical Report
-    05.12_, Faculty for Information and Communication Sciences, Hamburg
-    University of Technology, November 13, 2005.
-*/
-
-float nextAfter( float q ) {
-    // SmallestPositive is the smallest positive floating-point number.
-    static const float SmallestPositive = FLT_EPSILON * FLT_MIN;
-
-    /*  Scale is .625 ULP, so multiplying it by any significand in [1, 2)
-        yields something in [.625 ULP, 1.25 ULP].
-    */
-    static const float Scale = 0.625 * FLT_EPSILON;
-
-    // link with linker command line option -lm (Link Math library) otherwise fmax goes undefined
-    return q + fmax(SmallestPositive, fabs(q)*Scale);
-
-}//nextAfter
+#include <stdint.h>
 
 //-----------------------------------------------------------------------------
 
@@ -107,6 +57,10 @@ void integers( void ) {
 
 	size = sizeof( a );
 	size = sizeof( ub );
+
+	ub = (unsigned) 0xFFFFFFFF;
+	a  = (signed) ub;
+    ub = 0xFFFFFFFF;
 
 }//integers
 
@@ -171,6 +125,83 @@ void decimals_Double( void ) {
 
 
 }//decimals_Double
+
+//-----------------------------------------------------------------------------
+
+/*  Return the ULP of q.
+
+    This was inspired by Algorithm 3.5 in Siegfried M. Rump, Takeshi Ogita, and
+    Shin'ichi Oishi, "Accurate Floating-Point Summation", _Technical Report
+    05.12_, Faculty for Information and Communication Sciences, Hamburg
+    University of Technology, November 13, 2005.
+*/
+float ULP( float q ) {
+
+    // SmallestPositive is the smallest positive floating-point number.
+    static const float SmallestPositive = FLT_EPSILON * FLT_MIN;
+
+    /*  Scale is .75 ULP, so multiplying it by any significand in [1, 2) yields
+        something in [.75 ULP, 1.5 ULP) (even with rounding).
+    */
+    static const float Scale = 0.75 * FLT_EPSILON;
+
+    q = fabs(q);
+
+    // link with linker command line option -lm (Link Math library) otherwise fmax goes undefined
+    return fmax(SmallestPositive, q - (q - q * Scale));
+
+}//ULP
+
+//-----------------------------------------------------------------------------
+
+/*  Return the next floating-point value after the finite value q.
+
+    This was inspired by Algorithm 3.5 in Siegfried M. Rump, Takeshi Ogita, and
+    Shin'ichi Oishi, "Accurate Floating-Point Summation", _Technical Report
+    05.12_, Faculty for Information and Communication Sciences, Hamburg
+    University of Technology, November 13, 2005.
+*/
+
+float nextAfter( float q ) {
+    // SmallestPositive is the smallest positive floating-point number.
+    static const float SmallestPositive = FLT_EPSILON * FLT_MIN;
+
+    /*  Scale is .625 ULP, so multiplying it by any significand in [1, 2)
+        yields something in [.625 ULP, 1.25 ULP].
+    */
+    static const float Scale = 0.625 * FLT_EPSILON;
+
+    // link with linker command line option -lm (Link Math library) otherwise fmax goes undefined
+    return q + fmax(SmallestPositive, fabs(q)*Scale);
+
+}//nextAfter
+
+//-----------------------------------------------------------------------------
+
+unsigned long long floatSteps( void ) {
+
+	unsigned long long steps = 0;
+	long double next         = FLT_MIN;
+    float number             = .0F;
+    unsigned int diff        = 0;
+
+	for ( next = FLT_MIN, steps = 1; next < FLT_MAX; steps++ ) {
+
+		number = nextafterf( number, FLT_MAX );
+        if ( number == HUGE_VALF ) {
+           break;
+        }
+
+        next = number;
+
+	}//for
+
+	// 2^32 - (representable negative + positive float count)
+	diff = UINT_MAX - steps * 2;
+
+	return steps;
+
+}//floatSteps
 
 //-----------------------------------------------------------------------------
 
@@ -939,7 +970,7 @@ int dosomething( int a, int b ) {
 
 void dosomething_endmarker( void ) {
 
- 
+
 }//dosomething_endmarker
 
 void messWithTheFunctions() {
@@ -1996,6 +2027,9 @@ void structs( void ) {
 
 int main( int argc, char** argv ) {
 
+	floatSteps();
+
+	getLatinName( tuesday );
 
 	operators_conditionalExpression();
 	operators_additive();
